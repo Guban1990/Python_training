2559e7e52fe7eb129e584f23f7ad80d3ce9ff070
gubanov <gubanov@restream.rt.ru>
2020-10-11 18:59:49 +0300

    Урок №1 Задание N2

--- test_add_group.py
+++ test_add_group.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
+from group import Group
 
 class TestAddGroup(unittest.TestCase):
     def setUp(self):
@@ -17,7 +18,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="Test name", header= "Test header", footer="test comment")
+        self.create_group(wd, Group(name="Test name", header="Test header", footer="test comment"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -26,7 +27,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -38,22 +39,22 @@ class TestAddGroup(unittest.TestCase):
         # return to groups page
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
-        wd.find_element_by_name("selected[]").click()
+        # wd.find_element_by_name("selected[]").click()
         # fill group firm
-        wd.find_element_by_xpath("(//input[@name='delete'])[2]").click()
-        wd.find_element_by_link_text("groups").click()
+        # wd.find_element_by_xpath("(//input[@name='delete'])[2]").click()
+        # wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
         wd.find_element_by_link_text("home").click()
@@ -72,7 +73,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/index.php")
+        wd.get("http://localhost:8080/addressbook/index.php")
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
