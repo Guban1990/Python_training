1926e84b9cb6f3cf2f496cb4eb7bd10f4e4e0382
gubanov <gubanov@restream.rt.ru>
2020-11-07 22:06:49 +0300

    Урок №3 Задание №8 Оптимизировация ввода данных для group.py

--- test/test_edit_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="Test name2", header="Test header2", footer="test comment2"))
-    app.session.logout()
