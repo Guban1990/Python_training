44e6f298f7f1134fa9f3721be8f8658d46797797
gubanov <gubanov@restream.rt.ru>
2020-11-08 19:46:28 +0300

    Урок №3 Задание №8 Оптимизировация ввода данных для контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -5,15 +5,16 @@ class ContactHelper:
 
     def fill_contact_form(self, contact):
         wd = self.app.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("nickname", contact.nickname)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def return_to_home_page(self):
         # return to home page
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,5 +3,5 @@ from model.group import Contact
 
 def test_edit_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.edit_first_contact(Contact(firstname="Alex2", lastname="Gubanov2", nickname="Guban2"))
+    app.contact.edit_first_contact(Contact(firstname="New_firstname", lastname="New_lastname", nickname="New_nickname"))
     app.session.logout()
