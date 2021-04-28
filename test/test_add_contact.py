ce6a6eb0369aac7e6aa9decb0bb7900280385fc5
gubanov <gubanov@restream.rt.ru>
2021-04-29 00:57:06 +0300

    Урок №5 Задание №15 Добавлена параметризация при создании контакта, использовав случайно сгенерированные строки

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,4 +1,7 @@
 from model.group import Contact
+import pytest
+import random
+import string
 
 
 def test_add_contact(app):
@@ -12,7 +15,7 @@ def test_add_contact(app):
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-"""
+
 def test_add_empty_contact(app):
     old_contacts = app.contact.get_contact_list()
     contact = Contact(firstname="", lastname="", nickname="")
@@ -21,4 +24,26 @@ def test_add_empty_contact(app):
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
-"""
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", address="", homephone="", email="")] + [
+    Contact(firstname=random_string("firstname", 5), lastname=random_string("lastname", 10),
+            address=random_string("address", 20), homephone=(random_string("homephone", 10)),
+            email=(random_string("email", 7)))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
+    old_contacts = app.contact.get_contact_list()
+    app.contact.create(contact)
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
