c49f59543d92dc61ce39da84e76b64429ce4efd4
gubanov <gubanov@restream.rt.ru>
2021-08-19 23:31:33 +0300

    Урок №6 Задание №19 Реализована загрузка тестовых данных из файлов в формате JSON

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -23,8 +23,8 @@ def test_add_empty_contact(app):
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)"""
 
 
-def test_add_contact(app, data_contacts):
-    contact = data_contacts
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
